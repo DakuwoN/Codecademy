3bab3cbaa67db0f3cb90c081d3d3a37c1ac73e4b
Matthew McCane <MatthewLeeMcCane@gmail.com>
2023-12-13 14:05:01 -0500

    Passing Thoughts (React Hooks)

---
+++ Full Stack/React/Passing Thoughts/AddThoughtForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { generateId, getNewExpirationTime } from "./utilities";
+
+export function AddThoughtForm(props) {
+  const [text, setText] = useState("");
+
+  const handleTextchange = ({ target }) => {
+    const { value } = target;
+    setText(value);
+  };
+
+  const handleSubmit = (event) => {
+    if (text.length) {
+      event.preventDefault();
+      const thought = {
+        id: generateId(),
+        text: text,
+        expiresAt: getNewExpirationTime(),
+      };
+      props.addThought(thought);
+      setText("");
+    }
+  };
+
+  return (
+    <form className="AddThoughtForm" onSubmit={handleSubmit}>
+      <input
+        onChange={handleTextchange}
+        value={text}
+        type="text"
+        aria-label="What's on your mind?"
+        placeholder="What's on your mind?"
+      />
+      <input type="submit" value="Add" />
+    </form>
+  );
+}
