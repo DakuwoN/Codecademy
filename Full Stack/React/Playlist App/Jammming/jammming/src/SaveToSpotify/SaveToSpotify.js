a5c07ab32d45ddd7768feb43f0f111ec90839904
Matthew McCane <MatthewLeeMcCane@gmail.com>
2024-01-04 16:40:33 -0500

    Save to Spotify component

---
+++ Full Stack/React/Playlist App/Jammming/jammming/src/SaveToSpotify/SaveToSpotify.js
@@ -0,0 +1,8 @@
+import "./SaveToSpotify.css";
+
+function SaveToSpotify() {
+  console.log("App component is rendering");
+  return <button className="save-to-spotify">Save Playlist to Spotify</button>;
+}
+
+export default SaveToSpotify;
